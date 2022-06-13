9ca362b16311fb8287e249b20c42864539a4f4b0
AbdouTlili <ha_tlili@esi.dz>
2022-06-13 16:36:16 +0200

    working version with the gNB connected to the amf

--- openair2/RIC_AGENT/E2SM-MET/e2sm_met.c
+++ openair2/RIC_AGENT/E2SM-MET/e2sm_met.c
@@ -723,18 +723,8 @@ encode_met_Indication_Msg(ric_agent_info_t* ric, ric_subscription_t *rs)
 
     fprintf(stderr, "\n\n \n -*****------ gNB_ue_ngap_id %d, amf_ue_ngap_id %d, cqi %d, rsrp %d \n ",ue_context_p->ue_context.gNB_ue_ngap_id, ue_context_p->ue_context.amf_ue_ngap_id, UE_info->UE_sched_ctrl[k].CSI_report.cri_ri_li_pmi_cqi_report.wb_cqi_1tb, avg_rsrp);
 
-    {
-        const rnti_t rnti = UE_info->rnti[k];
-        NR_mac_stats_t *stats = &UE_info->mac_stats[k];
-        const int avg_rsrp = stats->num_rsrp_meas > 0 ? stats->cumul_rsrp / stats->num_rsrp_meas : 0;
-        struct rrc_gNB_ue_context_s         *ue_context_p = NULL;
-        ue_context_p = rrc_gNB_get_ue_context(RC.nrrrc[0], rnti);
-        fprintf(stderr,"\ngNB_ue_ngap_id %d, amf_ue_ngap_id %d, cqi %d, rsrp %d \n ",ue_context_p->ue_context.gNB_ue_ngap_id, ue_context_p->ue_context.amf_ue_ngap_id, UE_info->UE_sched_ctrl[k].CSI_report.cri_ri_li_pmi_cqi_report.wb_cqi_1tb, avg_rsrp);
 
-    }
-
-
-        ret = sprintf(tmp_char,"%lu",0);
+        ret = sprintf(tmp_char,"%lu",(unsigned long)ue_context_p->ue_context.amf_ue_ngap_id);
         amf_ue_ngap_id->buf  = (uint8_t *)strdup(tmp_char);
         amf_ue_ngap_id->size = strlen(tmp_char);
         ret = ASN_SEQUENCE_ADD(&tmp_meas_rec->measRecordItemList.list, amf_ue_ngap_id);
